e35b1126b5ecc22c8fa7444bfea6848972ed7075
itisameerkhan <itisameerkhan@gmail.com>
2024-05-02 08:59:41 +0530

    hero updated

--- Frontend/components/Header/Header.jsx
+++ Frontend/components/Header/Header.jsx
@@ -1,11 +1,64 @@
 import "./Header.scss";
+import aksvg from "../../src/assets/ak.svg";
 
 const Header = () => {
   return (
     <div className="header">
-      <a href="#home">Home</a>
-      <a href="#contact">contact</a>
-      <a href="#about">about</a>
+      <a href="#">
+        <img src={aksvg} alt="logo" />
+      </a>
+      <a href="#about">
+        <button>
+          <span className="span-mother">
+            <span>A</span>
+            <span>b</span>
+            <span>o</span>
+            <span>u</span>
+            <span>t</span>
+          </span>
+          <span className="span-mother2">
+            <span>A</span>
+            <span>b</span>
+            <span>o</span>
+            <span>u</span>
+            <span>t</span>
+          </span>
+        </button>
+      </a>
+      <button>
+        <span className="span-mother">
+          <span>S</span>
+          <span>k</span>
+          <span>i</span>
+          <span>l</span>
+          <span>l</span>
+          <span>s</span>
+        </span>
+        <span className="span-mother2">
+          <span>S</span>
+          <span>k</span>
+          <span>i</span>
+          <span>l</span>
+          <span>l</span>
+          <span>s</span>
+        </span>
+      </button>
+      <button>
+        <span className="span-mother">
+          <span>w</span>
+          <span>o</span>
+          <span>r</span>
+          <span>k</span>
+          <span>s</span>
+        </span>
+        <span className="span-mother2">
+          <span>w</span>
+          <span>o</span>
+          <span>r</span>
+          <span>k</span>
+          <span>s</span>
+        </span>
+      </button>
     </div>
   );
 };
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import About from "../components/About/About";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import "./App.scss";
@@ -5,8 +6,9 @@ import "./App.scss";
 const App = () => {
   return (
     <div className="app">
-      {/* <Header /> */}
+      <Header />
       <Hero />
+      <About />
     </div>
   );
 };
