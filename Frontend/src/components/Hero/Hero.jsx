afe76342cd37b377dcf277b6ea8795c8dadae645
itisameerkhan <itisameerkhan@gmail.com>
2024-05-29 20:22:55 +0530

    hero added

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import "./Hero.scss";
 import underline from "../../assets/underline.svg";
+import img1 from "../../imgs/img1.png";
+import img2 from "../../imgs/img2.jpg";
+import img3 from "../../imgs/img3.jpg";
+import img4 from "../../imgs/img4.jpg";
 
 const Hero = () => {
   return (
@@ -12,6 +16,12 @@ const Hero = () => {
         </span>{" "}
         Welcome to my corner of the internet!
       </p>
+      <div className="hero-img">
+        <img src={img1} alt="img" />
+        <img src={img2} alt="img" />
+        <img src={img3} alt="img" />
+        <img src={img4} alt="img" />
+      </div>
     </div>
   );
 };
