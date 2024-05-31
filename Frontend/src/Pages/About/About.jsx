d75201697bb3058ffa22577502145e46e5d5529d
itisameerkhan <itisameerkhan@gmail.com>
2024-05-31 09:48:53 +0530

    add

--- Frontend/src/Pages/About/About.jsx
+++ Frontend/src/Pages/About/About.jsx
@@ -2,8 +2,13 @@ import "./About.scss";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <div className="about-overview-1">
+        <p>Good morning!</p>
+        <p>I'm Ameer Khan, an MERN Stack developer.</p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
