b07aa7a558c88abbfcff8a434695622e63741e55
itisameerkhan <itisameerkhan@gmail.com>
2024-06-01 21:44:47 +0530

    add

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -1,12 +1,10 @@
 import "./Hero.scss";
 import underline from "../../assets/underline.svg";
-import img1 from "../../imgs/img1.png";
 import img2 from "../../imgs/img2.jpg";
 import img3 from "../../imgs/img3.jpg";
 import img4 from "../../imgs/img4.jpg";
 
 const Hero = () => {
-
   return (
     <div className="hero">
       <p className="hero-desc">
@@ -18,7 +16,10 @@ const Hero = () => {
         Welcome to my corner of the internet!
       </p>
       <div className="hero-img">
-        <img src={img1} alt="img"  />
+        <img
+          src="https://res.cloudinary.com/dnyehgbeu/image/upload/v1717145878/Portfolio/ydab9w2al9sqxeqb361v.jpg"
+          alt="img"
+        />
         <img src={img2} alt="img" />
         <img src={img3} alt="img" />
         <img src={img4} alt="img" />
