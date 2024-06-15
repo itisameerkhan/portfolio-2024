0bc1dddfeee520b029a5f2e1058f4712b56e161b
itisameerkhan <itisameerkhan@gmail.com>
2024-06-15 21:30:52 +0530

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,52 +1,16 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
 import Context from "./components/Context/Context";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Overview from "./components/Overview/Overview";
-import { helloArray } from "./utils/constants";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [displayHello, setdisplayHello] = useState("Hello");
-
-  useEffect(() => {
-    const handleLoad = () => setIsLoading(false);
-    const handleBeforeUnload = () => setIsLoading(true);
-
-    window.addEventListener("load", handleLoad);
-    window.addEventListener("<beforeunload", handleBeforeUnload);
-
-    const handleHello = () => {
-      setInterval(() => {
-        setdisplayHello(
-          helloArray[Math.floor(Math.random() * helloArray.length)]
-        );
-      }, 300);
-    };
-
-    handleHello();
-
-    return () => {
-      window.removeEventListener("load", handleLoad);
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
   return (
     <div className="app">
-      {isLoading ? (
-        <div className="hello">
-          <p>• {displayHello}</p>
-        </div>
-      ) : (
-        <div className="app">
-          <Hero />
-          <Context />
-          <Overview />
-          <Footer />
-        </div>
-      )}
+      <Hero />
+      <Context />
+      <Overview />
+      <Footer />
     </div>
   );
 };
