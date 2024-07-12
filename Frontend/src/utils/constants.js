f43ec1bb77575ae02c74477b91471b9719979d89
Ameer khan B <106725517+itisameerkhan@users.noreply.github.com>
2024-07-12 08:30:45 +0530

    Update constants.js

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -58,7 +58,7 @@ export const projectDescription = [
     title: "Grox Grocery App",
     description: "E-Commerce Grocery Application made with React and Firebase",
     img: "https://res.cloudinary.com/dnyehgbeu/image/upload/v1720707347/mockuper_9_nim7cf.png",
-    link:"grox.vercel.app"
+    link:"https://grox.vercel.app"
   },
 ];
 
