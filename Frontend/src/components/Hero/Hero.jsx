cc843cad3015dafdcdee162947da0b9b7f64b4c6
Ameer khan B <106725517+itisameerkhan@users.noreply.github.com>
2024-12-27 15:08:36 +0530

    Update Hero.jsx

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import underline from "../../assets/underline.svg";
 import img22 from "../../imgs/img22.jpg";
 import img23 from "../../imgs/img23.jpg";
 import img24 from "../../imgs/img24.jpg";
+import img25 from "../../imgs/img25.jpg";
 
 const Hero = () => {
   return (
@@ -17,7 +18,7 @@ const Hero = () => {
       </p>
       <div className="hero-img">
         <img
-          src="https://res.cloudinary.com/dnyehgbeu/image/upload/v1717145878/Portfolio/ydab9w2al9sqxeqb361v.jpg"
+          src={img25}
           alt="img"
         />
         <img src={img22} alt="img" />
